0f03b622644d0d822db37c66456abf31f7f4b7c3
Anthony Rurup <tcrurup@gmail.com>
2019-05-23 03:55:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,8 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last
     else
-      resp.write "404"
+      resp.write "Route not found"
+      resp.status = 404
     end
     
     resp.finish
