016ae04016c549e97d3337d788e3c77932c1c64e
Anthony Rurup <tcrurup@gmail.com>
2019-05-23 04:04:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_name = req.path.split(/items/).last
+      item_name = req.path.split("/items/").last
       binding.pry
       self.class.items.detect{ |item| item.name == item_name }.price
     else
